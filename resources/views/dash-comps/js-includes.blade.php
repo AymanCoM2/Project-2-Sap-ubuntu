ebe627e3694342fe852e5dcfff0fe05f358a2716
= <aymancoom3@gmail.com>
2023-09-06 16:26:07 +0300

    g

--- resources/views/dash-comps/js-includes.blade.php
+++ resources/views/dash-comps/js-includes.blade.php
@@ -1,17 +1,17 @@
-<script type="text/javascript" src={{ asset('js/jquery.js') }}></script>
-<script type="text/javascript" src={{ asset('js/bootstrap.bundle.js') }}></script>
-<script type="text/javascript" src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/j_jquery.dataTables.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/dataTables.buttons.min.js') }}"></script>
+<script type="text/javascript" src={{ mix('js/jquery.js') }}></script>
+<script type="text/javascript" src={{ mix('js/bootstrap.bundle.js') }}></script>
+<script type="text/javascript" src="{{ mix('js/jquery.dataTables.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/j_jquery.dataTables.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/dataTables.buttons.min.js') }}"></script>
 
-{{-- <script type="text/javascript" src="{{ asset('js/jquery-1.11.3.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jquery_11_dataTables.min.js') }}"></script> --}}
+{{-- <script type="text/javascript" src="{{ mix('js/jquery-1.11.3.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/jquery_11_dataTables.min.js') }}"></script> --}}
 
 
-<script type="text/javascript" src="{{ asset('js/buttons.html5.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/jszip.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/pdfmake.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/vfs_fonts.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/toastify-js.js') }}"></script>
-<script type="text/javascript" src="{{ asset('js/toastr.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/buttons.html5.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/jszip.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/pdfmake.min.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/vfs_fonts.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/toastify-js.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/toastr.min.js') }}"></script>
 {!! Toastr::message() !!}
